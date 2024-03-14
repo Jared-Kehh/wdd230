3d6feabb136a9df84525bf9616d6ca6edbcb068c
Jared Keh <keh19001@byui.edu>
2024-03-14 09:17:14 -0600

    weather api done

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,24 +1,20 @@
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=Diamond-Bar,06,+1&appid=13ab5a66f99f190dc9cdc5906e24c2bb')
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=34.00&lon=-117.81&appid=13ab5a66f99f190dc9cdc5906e24c2bb')
   .then(response => {
     // Check if the response is successful
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
     // Parse the JSON data returned by the API
     return response.json();
   })
   .then(data => {
     // Work with the JSON data here
     console.log(data);
+    const weather = document.getElementById("temp");
+    weather.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+    <p>${Math.round((data.main.temp - 273.15) * 9/5 + 32)}°F 
+    - ${data.weather[0].main}</p>`
   })
   .catch(error => {
     // Handle any errors that occurred during the fetch
     console.error('There was a problem with the fetch operation:', error);
   });
 
-  const weather = document.querySelector(".weather");
 
-  const weatherdiv = document.createElement('div');
-  weatherdiv.innerHTML = `
-  <p>${weather.temperature}</p>
-  `
